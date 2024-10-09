ce1bd49fc1e96958469008f49553cdeddf050182
Bartosh <b4rtosh@proton.me>
2024-10-09 20:27:42 +0200

    Add Docker config.

--- backend/aitranslator/settings.py
+++ backend/aitranslator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--@k^scb+-%k+f^a-f@zsn(*uut9#$l_oxmjczv+7$ov!(!mh1p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
